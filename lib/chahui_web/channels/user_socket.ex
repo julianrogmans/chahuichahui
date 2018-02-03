3d1f23ad24195d644a261d47419ce4c951dbcbb7
Julian Rogmans <rogmansj@gmail.com>
2018-02-02 20:33:20 -0800

    Hopefully final commit for successful deploy

--- lib/chahui_web/channels/user_socket.ex
+++ lib/chahui_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ChahuiWeb.UserSocket do
   # channel "room:*", ChahuiWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
